deaf02000e2f9ef5e347fc0c95de88f1789f1175
atom-x <faizjemal49@gmail.com>
2025-10-05 18:14:26 +0300

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Hamburger Menu Toggle with animation
+
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 
@@ -7,7 +7,6 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-// Smooth scroll for nav links
 document.querySelectorAll(".nav-links a").forEach(link => {
   link.addEventListener("click", e => {
     e.preventDefault();
@@ -20,10 +19,26 @@ document.querySelectorAll(".nav-links a").forEach(link => {
   });
 });
 
-// Form submit placeholder
 document.getElementById("commentForm")?.addEventListener("submit", e => {
   e.preventDefault();
   alert("Thank you! Message sent.");
   e.target.reset();
 });
- 
+  const observer = new IntersectionObserver((entries)=>{ 
+   entries.forEach((entry)=>{
+   if(entry.isIntersecting){
+   entry.target.classList.add('show')
+   console.log(entry.target)
+    }
+    else{
+      entry.target.classList.remove('show')
+    }
+  })
+  },{});
+  let classes = ["package-card","gallery-video","feature-card","testimonial-card"]
+ const cards = document.querySelectorAll('.package-card');
+  classes.forEach(el=>{
+document.querySelectorAll(`.${el}`).forEach(el => observer.observe(el));
+  }
+  )
+  document.getElementById("follow-us-section").querySelectorAll("a").forEach((el) => observer.observe(el));
